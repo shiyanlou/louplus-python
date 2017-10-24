eebd2272a44060cb61a4a0bf090d9afef628f0b0
aiden0z <aiden0xz@gmail.com>
2017-10-24 05:46:31 -0500

    update create_server.py

--- 18-create-servers-with-requests/create_server.py
+++ 18-create-servers-with-requests/create_server.py
@@ -4,10 +4,10 @@ CREATE_SERVER = 'http://127.0.0.1:5000/servers/'
 
 def create_server(name, host):
     data = {
-    # 删除所有服务器记录
         'name': name,
         'host': host
     }
+    # 思考：这里为什么没有设置 application/json 头部？
     resp = requests.post(CREATE_SERVER, json=data)
     return resp.json()
 
