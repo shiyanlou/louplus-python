99c47c7fe3e8a3d65bfbbbf9e66c698621f2e425
Jagger Wang <jaggerwang@gmail.com>
2018-08-22 13:46:04 +0800

    优化7-1

--- week7-1-co2-emissions-analysis/challenge7_1.py
+++ week7-1-co2-emissions-analysis/challenge7_1.py
@@ -3,12 +3,14 @@ import pandas as pd
 
 
 def data_clean():
-    data = pd.read_excel("ClimateChange.xlsx", sheetname='Data')
+    data = pd.read_excel("ClimateChange.xlsx", sheet_name='Data')
 
     # 处理 data 数据表 # 选取 EN.ATM.CO2E.KT 数据，并将国家代码设置为索引
-    data = data[data['Series code']== 'EN.ATM.CO2E.KT'].set_index('Country code')
+    data = data[data['Series code'] ==
+                'EN.ATM.CO2E.KT'].set_index('Country code')
     # 剔除不必要的数据列
-    data.drop(labels=['Country name', 'Series code', 'Series name', 'SCALE', 'Decimals'], axis=1, inplace=True)
+    data.drop(labels=['Country name', 'Series code',
+                      'Series name', 'SCALE', 'Decimals'], axis=1, inplace=True)
     # 将原数据集中不规范的空值替换为 NaN 方便填充
     data.replace({'..': pd.np.NaN}, inplace=True)
     # 对 NaN 空值进行向前和向后填充
@@ -20,15 +22,17 @@ def data_clean():
 
     # 处理 Country 数据表
     # 将国家代码设置为索引
-    countries =  pd.read_excel("ClimateChange.xlsx", sheetname='Country')
+    countries = pd.read_excel("ClimateChange.xlsx", sheet_name='Country')
     countries.set_index('Country code', inplace=True)
     # 剔除不必要的数据列
-    countries.drop(labels=['Capital city', 'Region', 'Lending category'], axis=1, inplace=True)
+    countries.drop(labels=['Capital city', 'Region',
+                           'Lending category'], axis=1, inplace=True)
 
     # 合并数据表
     # 对 Data 和 Country 表按照索引进行合并
-    return pd.concat([data, countries], axis=1)
+    return pd.concat([data, countries], axis=1, sort=True)
     # return pd.merge(pd.DataFrame(data), countries, left_index=True, right_index=True)
+
 
 def co2():
     '''co2() 函数用于数据统计，大致步骤如下：
@@ -43,15 +47,19 @@ def co2():
 
     # ERROR: df_max = df.max()
 
-    df_max = df.sort_values(by='Sum emissions', ascending=False).groupby('Income group').head(1).set_index('Income group')
+    df_max = df.sort_values(by='Sum emissions', ascending=False).groupby(
+        'Income group').head(1).set_index('Income group')
     df_max.columns = ['Highest emissions', 'Highest emission country']
-    df_max = df_max.reindex(columns=['Highest emission country', 'Highest emissions'])
+    df_max = df_max.reindex(
+        columns=['Highest emission country', 'Highest emissions'])
 
     # ERROR: df_min = df.min()
-    df_min = df.sort_values(by='Sum emissions').groupby('Income group').head(1).set_index('Income group')
+    df_min = df.sort_values(by='Sum emissions').groupby(
+        'Income group').head(1).set_index('Income group')
     df_min.columns = ['Lowest emissions', 'Lowest emission country']
-    df_min = df_min.reindex(columns=['Lowest emission country', 'Lowest emissions'])
+    df_min = df_min.reindex(
+        columns=['Lowest emission country', 'Lowest emissions'])
 
-    result = pd.concat([df_sum, df_max, df_min], axis=1)
+    result = pd.concat([df_sum, df_max, df_min], axis=1, sort=True)
 
     return result
