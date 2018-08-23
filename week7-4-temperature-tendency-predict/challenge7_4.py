d492a54196e8d15a301cc611dc3bfdcf6836d14f
Jagger Wang <jaggerwang@gmail.com>
2018-08-23 17:25:32 +0800

    PEP8格式化7-4

--- week7-4-temperature-tendency-predict/challenge7_4.py
+++ week7-4-temperature-tendency-predict/challenge7_4.py
@@ -10,25 +10,34 @@ from sklearn.linear_model import LinearRegression
 def Temperature():
     # 读取数据集
     df_GreenhouseGas = pd.read_csv('GreenhouseGas.csv', header=0)
-    df_GlobalSurfaceTemperature = pd.read_csv('GlobalSurfaceTemperature.csv', header=0)
+    df_GlobalSurfaceTemperature = pd.read_csv(
+        'GlobalSurfaceTemperature.csv', header=0)
     df_CO2ppm = pd.read_csv('CO2ppm.csv')
 
     # 整理对齐数据集
-    df_GreenhouseGas_new = pd.DataFrame(df_GreenhouseGas[['N2O', 'CH4', 'CO2']].values,
-        index=pd.to_datetime(df_GreenhouseGas['Year'].astype(str)),
+    df_GreenhouseGas_new = pd.DataFrame(
+        df_GreenhouseGas[['N2O', 'CH4', 'CO2']].values,
+        index=pd.to_datetime(
+            df_GreenhouseGas['Year'].astype(str)),
         columns=['N2O', 'CH4', 'CO2']
-        )
-    df_GlobalSurfaceTemperature_new = pd.DataFrame(df_GlobalSurfaceTemperature[['Median', 'Upper', 'Lower']].values,
-        index=pd.to_datetime(df_GlobalSurfaceTemperature['Year'].astype(str)),
-        columns=['Median', 'Upper', 'Lower']
-        )
+    )
+    df_GlobalSurfaceTemperature_new = pd.DataFrame(
+        df_GlobalSurfaceTemperature[['Median', 'Upper', 'Lower']].values,
+        index=pd.to_datetime(
+            df_GlobalSurfaceTemperature['Year'].astype(str)),
+        columns=[
+            'Median', 'Upper', 'Lower']
+    )
     df_CO2ppm_new = pd.DataFrame(df_CO2ppm.iloc[:, 1].values,
-        index=pd.to_datetime(df_CO2ppm['Year'].astype(str)),
-        columns=['CO2_PPM'])
-    df_merge = pd.concat([df_GreenhouseGas_new, df_CO2ppm_new, df_GlobalSurfaceTemperature_new], axis=1)
+                                 index=pd.to_datetime(
+                                     df_CO2ppm['Year'].astype(str)),
+                                 columns=['CO2_PPM'])
+    df_merge = pd.concat(
+        [df_GreenhouseGas_new, df_CO2ppm_new, df_GlobalSurfaceTemperature_new], axis=1)
 
     # 数据填充
-    feature = df_merge.iloc[:, 0:4].fillna(method='ffill').fillna(method='bfill')
+    feature = df_merge.iloc[:, 0:4].fillna(
+        method='ffill').fillna(method='bfill')
     feature_train = feature['1970-01-01':'2010-01-01']
     feature_test = feature['2011-01-01':'2017-01-01']
 
